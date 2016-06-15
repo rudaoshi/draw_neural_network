a655feb21aad895ae9521e9cdb806108057f5fa4
rudaoshi <sunmingming@gmail.com>
2016-06-15 13:09:30 +0800

    add

--- draw_conv_1d.py
+++ draw_conv_1d.py
@@ -25,10 +25,26 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.transforms
 
 sns.set(style="white")
 
-def add_tensor_2d(ax, length = 10, feature_num = 5, notification_box = None, x_label = None, y_label = None):
+
+def calculate_region(box, width, height, cel_size = 0.01):
+
+    if isinstance(box, list):
+        center = (box[0] + box[2]/2, box[1], box[3]/2)
+    elif isinstance(box, matplotlib.transforms.Bbox):
+        center = (box.x0 + box.width/2, box.y0, box.height/2)
+    else:
+        raise Exception("unknown box type")
+
+    region = (center[0] - width/2.0*cel_size, center[1] - height/2.0 * cel_size, width * cel_size, height*cel_size)
+
+    return region
+
+
+def add_tensor_2d(box, length = 10, feature_num = 5, notification_box = None, x_label = None, y_label = None):
 
     # # Generate a large random dataset
     # rs = np.random.RandomState(33)
@@ -41,23 +57,27 @@ def add_tensor_2d(ax, length = 10, feature_num = 5, notification_box = None, x_l
     uniform_data = np.random.randn(feature_num, length)
     # Draw the heatmap with the mask and correct aspect ratio
 
-    sns.heatmap(uniform_data, xticklabels = False, yticklabels = False, square=True,
-                linewidths=.5, ax = ax, cbar = False)
+    region = calculate_region(box, length, feature_num)
+    cur_ax = plt.gcf().add_axes(region)
 
-    ax.add_patch(Rectangle((0,0), length, feature_num, fill=False, color="black", linewidth=2))
+    sns.heatmap(uniform_data, xticklabels = False, yticklabels = False, square=True,
+                linewidths=.5, ax = cur_ax, cbar = False)
+
+    cur_ax.add_patch(Rectangle((0,0), length, feature_num, fill=False, color="black", linewidth=2))
 
     if not notification_box:
         notification_box = []
 
     for box in notification_box:
-        ax.add_patch(Rectangle(box["box"][:2], box["box"][2], box["box"][3], fill=True, color=box["color"], alpha=box["alpha"], linewidth=5))
+        cur_ax.add_patch(Rectangle(box["box"][:2], box["box"][2], box["box"][3], fill=True, color=box["color"], alpha=box["alpha"], linewidth=5))
+
+    return cur_ax
+
+import copy
+def add_tensor_3d(box, layer_num, length = 10, feature_num = 5, shift = (0.1, 0.1), notification_box = None, x_label = None, y_label = None):
 
 
-def add_tensor_3d(gs, layer_num, length = 10, feature_num = 5, shift = (0.1, 0.1), notification_box = None, x_label = None, y_label = None):
-
-    ax_box = gs.get_position(plt.gcf())
-
-    pos = [ax_box.x0, ax_box.y0, ax_box.width - (layer_num-1)*shift[0], ax_box.height - (layer_num-1)*shift[1]]
+    pos = [box.x0, box.y0, box.width , box.height ]
 
     if not notification_box:
         notification_box = []
@@ -66,17 +86,18 @@ def add_tensor_3d(gs, layer_num, length = 10, feature_num = 5, shift = (0.1, 0.1
 
     axes = []
     for i in range(layer_num):
+        print pos
 
-        cur_ax = plt.gcf().add_axes(pos)
 
-        axes.append(cur_ax)
+
         if notification_box:
             cur_boxes = notification_box[i]
         else:
             cur_boxes = None
 
-        add_tensor_2d(cur_ax, length, feature_num, x_label = x_label, y_label = y_label, notification_box=cur_boxes)
 
+        cur_ax = add_tensor_2d(pos, length, feature_num, x_label = x_label, y_label = y_label, notification_box=cur_boxes)
+        axes.append(cur_ax)
         x_label = None
         y_label = None
 
@@ -97,146 +118,11 @@ def add_connection(source_ax, source_data_coord,  target_ax, target_data_coord):
 
     source_ax.add_artist(con)
 
-    # inv = plt.gcf().transFigure.inverted()
-    #
-    # source_disp_coord = source_ax.transData.transform(source_data_coord)
-    # print source_disp_coord
-    # source_fig_coord = inv.transform(source_disp_coord)
-    # print source_fig_coord
-    #
-    # target_disp_coord = target_ax.transData.transform(target_data_coord)
-    # print target_disp_coord
-    # target_fig_coord = inv.transform(target_disp_coord)
-    # print target_fig_coord
-    #
-    # line = matplotlib.lines.Line2D((source_fig_coord[0], target_fig_coord[0]), (source_fig_coord[1], target_fig_coord[1]),
-    #                                transform=plt.gcf().transFigure)
-    #
-    # plt.gcf().lines = line,
-#    plt.gcf().line(source_fig_coord[0], source_fig_coord[1],
-#                    target_fig_coord[0], target_fig_coord[1], head_width=0.05, head_length=0.1, fc='k', ec='k')
-
-#    plt.gcf().line(0, 0, 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
-
-def add_mapping(patches, colors, start_ratio, patch_size, ind_bgn,
-                top_left_list, loc_diff_list, num_show_list, size_list):
-
-    start_loc = top_left_list[ind_bgn] \
-        + (num_show_list[ind_bgn] - 1) * np.array(loc_diff_list[ind_bgn]) \
-        + np.array([start_ratio[0] * size_list[ind_bgn],
-                    -start_ratio[1] * size_list[ind_bgn]])
-
-    end_loc = top_left_list[ind_bgn + 1] \
-        + (num_show_list[ind_bgn + 1] - 1) \
-        * np.array(loc_diff_list[ind_bgn + 1]) \
-        + np.array([(start_ratio[0] + .5 * patch_size / size_list[ind_bgn]) *
-                    size_list[ind_bgn + 1],
-                    -(start_ratio[1] - .5 * patch_size / size_list[ind_bgn]) *
-                    size_list[ind_bgn + 1]])
-
-    patches.append(Rectangle(start_loc, patch_size, patch_size))
-    colors.append(Dark)
-    patches.append(Line2D([start_loc[0], end_loc[0]],
-                          [start_loc[1], end_loc[1]]))
-    colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
-                          [start_loc[1], end_loc[1]]))
-    colors.append(Black)
-    patches.append(Line2D([start_loc[0], end_loc[0]],
-                          [start_loc[1] + patch_size, end_loc[1]]))
-    colors.append(Black)
-    patches.append(Line2D([start_loc[0] + patch_size, end_loc[0]],
-                          [start_loc[1] + patch_size, end_loc[1]]))
-    colors.append(Black)
-
-
-def label(xy, text, xy_off=[0, 4]):
-    plt.text(xy[0] + xy_off[0], xy[1] + xy_off[1], text,
-             family='sans-serif', size=8)
-
-
-def draw():
-    fc_unit_size = 2
-    layer_width = 40
-
-    patches = []
-    colors = []
-
-    fig, ax = plt.subplots()
-
-
-    ############################
-    # conv layers
-    size_list = [32, 18, 10, 6, 4]
-    num_list = [3, 32, 32, 48, 48]
-    x_diff_list = [0, layer_width, layer_width, layer_width, layer_width]
-    text_list = ['Inputs'] + ['Feature\nmaps'] * (len(size_list) - 1)
-    loc_diff_list = [[3, -3]] * len(size_list)
-
-    num_show_list = map(min, num_list, [NumConvMax] * len(num_list))
-    top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
-
-    for ind in range(len(size_list)):
-        position = [ind * 100, 0, 100, 100]
-        add_tensor_2d(poistion=position)
-        label(top_left_list[ind], text_list[ind] + '\n{}@{}x{}'.format(
-            num_list[ind], size_list[ind], size_list[ind]))
-
-
-    ############################
-    # in between layers
-    start_ratio_list = [[0.4, 0.5], [0.4, 0.8], [0.4, 0.5], [0.4, 0.8]]
-    patch_size_list = [5, 2, 5, 2]
-    ind_bgn_list = range(len(patch_size_list))
-    text_list = ['Convolution', 'Max-pooling', 'Convolution', 'Max-pooling']
-
-    for ind in range(len(patch_size_list)):
-        add_mapping(patches, colors, start_ratio_list[ind],
-                    patch_size_list[ind], ind,
-                    top_left_list, loc_diff_list, num_show_list, size_list)
-        label(top_left_list[ind], text_list[ind] + '\n{}x{} kernel'.format(
-            patch_size_list[ind], patch_size_list[ind]), xy_off=[26, -65])
-
-
-    ############################
-    # fully connected layers
-    size_list = [fc_unit_size, fc_unit_size, fc_unit_size]
-    num_list = [768, 500, 2]
-    num_show_list = map(min, num_list, [NumFcMax] * len(num_list))
-    x_diff_list = [sum(x_diff_list) + layer_width, layer_width, layer_width]
-    top_left_list = np.c_[np.cumsum(x_diff_list), np.zeros(len(x_diff_list))]
-    loc_diff_list = [[fc_unit_size, -fc_unit_size]] * len(top_left_list)
-    text_list = ['Hidden\nunits'] * (len(size_list) - 1) + ['Outputs']
-
-    for ind in range(len(size_list)):
-        position = [1000+ind * 100, 0, 100, 100]
-        add_tensor_2d(position)
-        label(top_left_list[ind], text_list[ind] + '\n{}'.format(
-            num_list[ind]))
-
-    text_list = ['Flatten\n', 'Fully\nconnected', 'Fully\nconnected']
-
-    for ind in range(len(size_list)):
-        label(top_left_list[ind], text_list[ind], xy_off=[-10, -65])
-
-    ############################
-    colors += [0, 1]
-    collection = PatchCollection(patches, cmap=plt.cm.gray)
-    collection.set_array(np.array(colors))
-    ax.add_collection(collection)
-#    plt.tight_layout()
-    plt.axis('equal')
-    plt.axis('off')
-    plt.show()
-#    fig.set_size_inches(8, 2.5)
-
-    fig_dir = './'
-    fig_ext = '.png'
-    fig.savefig(os.path.join(fig_dir, 'convnet_fig' + fig_ext),
-                bbox_inches='tight', pad_inches=0)
 
 
 import matplotlib.gridspec as gridspec
+
+
 
 if __name__ == "__main__":
 
@@ -244,6 +130,8 @@ if __name__ == "__main__":
     text_length = 20
     embedding_dim = 5
     conv_dim = 10
+
+    feature_component_dim = 10
 
     embedding_layer_pos = [0, 0, 1, 1]
     #
@@ -253,46 +141,85 @@ if __name__ == "__main__":
     # fig_ext = '.png'
     # f.savefig(os.path.join(fig_dir, 'embedding_layer' + fig_ext),
     #             bbox_inches='tight', pad_inches=0)
-    gs = gridspec.GridSpec(4, 1)
 
-    embedding_ax = plt.subplot(gs[3, :])
-    note_boxes = [{"box": [1.5,0, 3, embedding_dim], "color":"blue", "alpha" : 0.6},
-                  {"box": [7.5,0, 3, embedding_dim], "color":"red", "alpha" : 0.6},
-                  {"box": [16.5, 0, 3, embedding_dim], "color": "green", "alpha": 0.6}]
-    add_tensor_2d(embedding_ax, length=text_length, feature_num=embedding_dim, x_label="words", y_label= "embedding", notification_box=note_boxes)
+    plt.gcf().set_size_inches(16, 10)
+    gs = gridspec.GridSpec(3, 7)
+
+    #embedding_ax = plt.subplot(gs[3, 0])
+    note_boxes = [{"box": [0.5,0, 2, embedding_dim], "color":"blue", "alpha" : 0.6},
+                  {"box": [9.5,0, 2, embedding_dim], "color":"red", "alpha" : 0.6},
+                  {"box": [16.5, 0, 2, embedding_dim], "color": "yellow", "alpha": 0.6}]
+    embedding_ax = add_tensor_2d(gs[2, 0].get_position(plt.gcf()), length=text_length, feature_num=embedding_dim, x_label="words", y_label= "embedding", notification_box=note_boxes)
 
 
 #    conv_ax = plt.subplot(gs[1:3, :])
 
-    shift = [0.1,0.025]
+    shift = [0.025,0.025]
 
-    note_boxes = [[{"box": [1, 0, 1, conv_dim], "color": "blue", "alpha": 0.6}, {"box": [0, 8, text_length, 1], "color": "cyan", "alpha": 0.6}, ],
-                  [{"box": [4, 0, 1, conv_dim], "color": "red", "alpha": 0.6}, {"box": [0, 5, text_length, 1], "color": "magenta", "alpha": 0.6},],
-                  [{"box": [16, 0, 1, conv_dim], "color": "green", "alpha": 0.6}, {"box": [0, 2, text_length, 1], "color": "orange", "alpha": 0.6},]]
-    conv_axes = add_tensor_3d(gs[1:3, :], 3, length=text_length, feature_num=conv_dim, shift = shift, notification_box=note_boxes)
+    note_boxes = [[{"box": [1, 0, 1, conv_dim], "color": "blue", "alpha": 0.6}, {"box": [0, 16, text_length, 1], "color": "cyan", "alpha": 0.6}, ],
+                  [{"box": [4, 0, 1, conv_dim], "color": "red", "alpha": 0.6}, {"box": [0, 12, text_length, 1], "color": "magenta", "alpha": 0.6},],
+                  [{"box": [16, 0, 1, conv_dim], "color": "yellow", "alpha": 0.6}, {"box": [0, 3, text_length, 1],
+                                                                                "color": "orange", "alpha": 0.6}]]
+
+    conv_axes = add_tensor_3d(gs[1, 0].get_position(plt.gcf()), 3, length=text_length, feature_num=conv_dim, shift = shift, notification_box=note_boxes)
 
 
+#    add_connection(embedding_ax, (1.5, embedding_dim), conv_axes[0] , (1.5, 0))
+#    add_connection(embedding_ax, (4.5, embedding_dim), conv_axes[0], (1.5, 0))
+#    add_connection(embedding_ax, (7.5, embedding_dim), conv_axes[1], (4.5, 0))
+#    add_connection(embedding_ax, (10.5, embedding_dim), conv_axes[1], (4.5, 0))
 
-#    add_connection(embedding_ax, (3, embedding_dim), conv_axes[0] , (1.5, 0))
-#    add_connection(embedding_ax, (9, embedding_dim), conv_axes[1], (4.5, 0))
-#    add_connection(embedding_ax, (18, embedding_dim), conv_axes[2], (16.5, 0))
 
-    max_pool_ax = plt.subplot(gs[0, :])
     note_boxes = [{"box": [5, 0, 1, 1], "color": "cyan", "alpha": 0.6},
                   {"box": [16, 0, 1, 1], "color": "magenta", "alpha": 0.6},
                   {"box": [35, 0, 1, 1], "color": "orange", "alpha": 0.6}]
-    add_tensor_2d(max_pool_ax, length=text_length*2, feature_num=1, notification_box=note_boxes)
+    max_pool_ax = add_tensor_2d(gs[0, 0].get_position(plt.gcf()), length=feature_component_dim, feature_num=1, notification_box=note_boxes)
 
 
 #    add_connection(conv_axes[0], (0, 8.5), max_pool_ax , (5.5, 0))
 #    add_connection(conv_axes[1], (0, 5.5), max_pool_ax, (16.5, 0))
-#    add_connection(conv_axes[2], (text_length, 2.5), max_pool_ax, (35.5, 0))
+
+    # embedding_ax = plt.subplot(gs[3, 0])
+    note_boxes = [{"box": [0.5, 0, 2, embedding_dim], "color": "blue", "alpha": 0.6},
+                  {"box": [9.5, 0, 2, embedding_dim], "color": "red", "alpha": 0.6},
+                  {"box": [16.5, 0, 2, embedding_dim], "color": "yellow", "alpha": 0.6}]
+    embedding_ax = add_tensor_2d(gs[2, 6].get_position(plt.gcf()), length=text_length, feature_num=embedding_dim,
+                                 x_label="words", y_label="embedding", notification_box=note_boxes)
+
+    #    conv_ax = plt.subplot(gs[1:3, :])
+
+    shift = [0.025, 0.025]
+
+    note_boxes = [[{"box": [1, 0, 1, conv_dim], "color": "blue", "alpha": 0.6},
+                   {"box": [0, 16, text_length, 1], "color": "cyan", "alpha": 0.6}, ],
+                  [{"box": [4, 0, 1, conv_dim], "color": "red", "alpha": 0.6},
+                   {"box": [0, 12, text_length, 1], "color": "magenta", "alpha": 0.6}, ],
+                  [{"box": [16, 0, 1, conv_dim], "color": "yellow", "alpha": 0.6}, {"box": [0, 3, text_length, 1],
+                                                                                    "color": "orange", "alpha": 0.6}]]
+
+    conv_axes = add_tensor_3d(gs[1, 6].get_position(plt.gcf()), 3, length=text_length, feature_num=conv_dim,
+                              shift=shift, notification_box=note_boxes)
+
+    #    add_connection(embedding_ax, (1.5, embedding_dim), conv_axes[0] , (1.5, 0))
+    #    add_connection(embedding_ax, (4.5, embedding_dim), conv_axes[0], (1.5, 0))
+    #    add_connection(embedding_ax, (7.5, embedding_dim), conv_axes[1], (4.5, 0))
+    #    add_connection(embedding_ax, (10.5, embedding_dim), conv_axes[1], (4.5, 0))
 
 
-    plt.gcf().tight_layout()
-    plt.show()
+    note_boxes = [{"box": [5, 0, 1, 1], "color": "cyan", "alpha": 0.6},
+                  {"box": [16, 0, 1, 1], "color": "magenta", "alpha": 0.6},
+                  {"box": [35, 0, 1, 1], "color": "orange", "alpha": 0.6}]
+    max_pool_ax = add_tensor_2d(gs[0, 6].get_position(plt.gcf()), length=feature_component_dim, feature_num=1,
+                                notification_box=note_boxes)
 
-    # fig_dir = './'
-    # fig_ext = '.png'
-    # plt.gcf().savefig(os.path.join(fig_dir, 'language_model.'+ fig_ext),
-    #            pad_inches=0)
+    for i in range(1,6):
+        add_tensor_2d(gs[0, i].get_position(plt.gcf()), length=feature_component_dim, feature_num=1)
+
+
+    #    plt.gcf().tight_layout()
+    #plt.show()
+
+    fig_dir = './'
+    fig_ext = '.png'
+    plt.gcf().savefig(os.path.join(fig_dir, 'language_model.'+ fig_ext),
+                pad_inches=0)
